a7b1dcfb1fec23be64efc1e48e79d659bc5212e6
Peter Eliasson <warackta@gmail.com>
2016-04-14 15:00:27 +0200

    Created NotesActivity

---
+++ app/src/main/java/group8/eda397/chalmers/se/pairprogramming/notes/NotesActivity.java
@@ -0,0 +1,17 @@
+package group8.eda397.chalmers.se.pairprogramming.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import group8.eda397.chalmers.se.pairprogramming.R;
+
+public class NotesActivity extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
