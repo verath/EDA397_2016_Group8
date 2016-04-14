aebf1baed09ec359b8872d95d11fad63d6c58f98
Peter Eliasson <warackta@gmail.com>
2016-04-14 21:00:44 +0200

    Changed MainActivity to use the AppBarLayout.

--- app/src/main/java/group8/eda397/chalmers/se/pairprogramming/MainActivity.java
+++ app/src/main/java/group8/eda397/chalmers/se/pairprogramming/MainActivity.java
@@ -2,6 +2,7 @@ package group8.eda397.chalmers.se.pairprogramming;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
