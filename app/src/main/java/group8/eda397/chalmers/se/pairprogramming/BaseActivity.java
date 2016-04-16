03b3d34ef57ae60b464dd3a7e41917c27ba24e6a
Peter Eliasson <warackta@gmail.com>
2016-04-16 13:23:02 +0200

    Added a BaseActivity for shared activity logic.

---
+++ app/src/main/java/group8/eda397/chalmers/se/pairprogramming/BaseActivity.java
@@ -0,0 +1,43 @@
+package group8.eda397.chalmers.se.pairprogramming;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    protected void addFragment(int containerId, Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(containerId, fragment);
+        fragmentTransaction.commit();
+    }
+
+    protected Fragment findFragment(int containerId) {
+        FragmentManager fm = getSupportFragmentManager();
+        return fm.findFragmentById(containerId);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
