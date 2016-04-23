6dcad341c409251f8f29901ad554a876804d40f0
Peter Eliasson <warackta@gmail.com>
2016-04-23 15:10:22 +0200

    Moves TimerService to the foreground if active.
    This prevents the system from killing our service. This also requires us
    to display a notification, which we do.

--- app/src/main/java/group8/eda397/chalmers/se/pairprogramming/timer/TimerService.java
+++ app/src/main/java/group8/eda397/chalmers/se/pairprogramming/timer/TimerService.java
@@ -1,5 +1,8 @@
 package group8.eda397.chalmers.se.pairprogramming.timer;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +10,10 @@ import android.os.Binder;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+import group8.eda397.chalmers.se.pairprogramming.R;
 
 /**
  * A service for running a CountDownTimer that may live for longer than the
@@ -18,10 +25,14 @@ import android.support.annotation.Nullable;
  */
 public class TimerService extends Service {
 
+    public static final int TIMER_NOTIFICATION_ID = 1;
+
     public interface Listener {
+
         void onTimerTick(long millisUntilFinished);
 
         void onTimerFinish();
+
     }
 
     public static Intent getStartingIntent(Context context) {
@@ -29,14 +40,41 @@ public class TimerService extends Service {
     }
 
     private final IBinder mBinder = new TimerServiceBinder();
+
+    private NotificationManager mNotificationManager;
     private CountDownTimer mCountDownTimer;
     private Listener mListener;
+
+    private boolean mNotificationShown;
     private long mMillisUntilFinished = 0;
-    private boolean mFinished = true;
+    private boolean mTimerFinished = true;
+
+    @Override
+    public void onCreate() {
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
         return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        if (mTimerFinished) {
+            // No timer running, so stop ourselves
+            stopSelf();
+            return false;
+        } else {
+            // Timer is running, start ourselves as a foreground service
+            moveToForeground();
+            return true;
+        }
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        moveToBackground();
     }
 
     public void setListener(@Nullable Listener listener) {
@@ -49,7 +87,7 @@ public class TimerService extends Service {
         }
         cancelTimer();
 
-        mFinished = false;
+        mTimerFinished = false;
         mMillisUntilFinished = millisInFuture;
 
         mCountDownTimer = new CountDownTimerImpl(millisInFuture, 500);
@@ -57,7 +95,7 @@ public class TimerService extends Service {
     }
 
     public void cancelTimer() {
-        mFinished = true;
+        mTimerFinished = true;
         mMillisUntilFinished = 0;
         if (mCountDownTimer != null) {
             mCountDownTimer.cancel();
@@ -70,15 +108,81 @@ public class TimerService extends Service {
     }
 
     public boolean isFinished() {
-        return mFinished;
+        return mTimerFinished;
     }
 
+    private void onTimerTick(long millisUntilFinished) {
+        mMillisUntilFinished = millisUntilFinished;
+        if (mListener != null) {
+            mListener.onTimerTick(millisUntilFinished);
+        }
+        updateNotification();
+    }
+
+    private void onTimerFinish() {
+        mTimerFinished = true;
+        if (mListener != null) {
+            mListener.onTimerFinish();
+        }
+        updateNotification();
+    }
+
+    private void updateNotification() {
+        if (mNotificationShown) {
+            Notification newNotification = createTimerNotification();
+            mNotificationManager.notify(TIMER_NOTIFICATION_ID, newNotification);
+        }
+    }
+
+    private void moveToForeground() {
+        mNotificationShown = true;
+        Notification notification = createTimerNotification();
+        startForeground(TIMER_NOTIFICATION_ID, notification);
+    }
+
+    private Notification createTimerNotification() {
+        PendingIntent contentIntent = createNotificationContentIntent();
+        return new NotificationCompat.Builder(this)
+                .setContentTitle("Timer")
+                .setContentText(String.format("%d", mMillisUntilFinished))
+                .setSmallIcon(R.drawable.ic_add_white_24dp)
+                .setContentIntent(contentIntent)
+                .build();
+    }
+
+    private PendingIntent createNotificationContentIntent() {
+        // See http://developer.android.com/guide/topics/ui/notifiers/notifications.html#DirectEntry
+        Intent timerIntent = TimerActivity.getCallingIntent(this);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(TimerActivity.class);
+        stackBuilder.addNextIntent(timerIntent);
+        return stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private void moveToBackground() {
+        mNotificationShown = false;
+        stopForeground(true);
+    }
+
+    /**
+     * The Binder used when binding to the TimerService, simply
+     * exposing a method for getting the TimerService instance.
+     */
     public class TimerServiceBinder extends Binder {
+        /**
+         * Gets the TimerService instance.
+         *
+         * @return The TimerService instance.
+         */
         TimerService getTimerService() {
             return TimerService.this;
         }
     }
 
+    /**
+     * Simple implementation of CountDownTimer that forwards
+     * any callbacks to the TimerService.
+     */
     private class CountDownTimerImpl extends CountDownTimer {
         public CountDownTimerImpl(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);
@@ -86,18 +190,12 @@ public class TimerService extends Service {
 
         @Override
         public void onTick(long millisUntilFinished) {
-            mMillisUntilFinished = millisUntilFinished;
-            if (mListener != null) {
-                mListener.onTimerTick(millisUntilFinished);
-            }
+            onTimerTick(millisUntilFinished);
         }
 
         @Override
         public void onFinish() {
-            mFinished = true;
-            if (mListener != null) {
-                mListener.onTimerFinish();
-            }
+            onTimerFinish();
         }
     }
 }
