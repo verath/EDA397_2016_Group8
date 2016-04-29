4030529fd49df7bb3799dc2249b9bb8ba3b70078
Calleberg <martin.calleberg@gmail.com>
2016-04-29 11:55:37 +0200

    Add edit backlog item (#17)
    Backlog items added to repository and changes saved after editing

---
+++ app/src/main/java/se/chalmers/eda397/group8/pairprogramming/backlog/addedit/AddEditBacklogPresenter.java
@@ -0,0 +1,61 @@
+package se.chalmers.eda397.group8.pairprogramming.backlog.addedit;
+
+import android.support.annotation.NonNull;
+
+import se.chalmers.eda397.group8.pairprogramming.backlog.model.BacklogItem;
+import se.chalmers.eda397.group8.pairprogramming.backlog.model.BacklogItemDataSource;
+
+public class AddEditBacklogPresenter implements AddEditBacklogContract.Presenter {
+
+    private final AddEditBacklogContract.View mBacklogView;
+    private final BacklogItemDataSource mDataSource;
+    private final String mItemId;
+    private final BacklogItem.Status mDefaultStatus;
+
+    public AddEditBacklogPresenter(AddEditBacklogContract.View backlogView, String itemId,
+                                   BacklogItem.Status defaultStatus, BacklogItemDataSource dataSource) {
+        this.mBacklogView = backlogView;
+        this.mDataSource = dataSource;
+        this.mItemId = itemId;
+        this.mDefaultStatus = defaultStatus;
+        mBacklogView.setPresenter(this);
+    }
+
+    @Override
+    public void start() {
+        if (mItemId != null) {
+            BacklogItem item = mDataSource.get(mItemId);
+            if (item == null) {
+                mBacklogView.showMissingBacklogItem();
+            } else {
+                populateFields(item);
+            }
+        } else if (mDefaultStatus != null) {
+            mBacklogView.showStatus(mDefaultStatus);
+        }
+    }
+
+    private void populateFields(BacklogItem item) {
+        mBacklogView.showTitle(item.getTitle());
+        mBacklogView.showContent(item.getContent());
+        mBacklogView.showStatus(item.getStatus());
+    }
+
+    @Override
+    public void onSaveItem(@NonNull String title, @NonNull String content,
+                           @NonNull BacklogItem.Status status) {
+        if (title.isEmpty()) {
+            mBacklogView.showTitleEmptyError();
+            return;
+        }
+
+        BacklogItem item;
+        if (mItemId != null) {
+            item = new BacklogItem(mItemId, title, content, status);
+        } else {
+            item = new BacklogItem(title, content, status);
+        }
+        mDataSource.save(item);
+        mBacklogView.showBacklog();
+    }
+}
